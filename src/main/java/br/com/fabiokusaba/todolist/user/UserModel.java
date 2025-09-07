ba329d964268c530b9042157fe38bb1811e6e36a
Fabio Kusaba <fabinhokb44@gmail.com>
2025-09-07 10:33:08 -0300

    feat: add unique constraint to username column

--- src/main/java/br/com/fabiokusaba/todolist/user/UserModel.java
+++ src/main/java/br/com/fabiokusaba/todolist/user/UserModel.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,7 +21,10 @@ public class UserModel {
     private UUID id;
 
     private String name;
+
+    @Column(unique = true)
     private String username;
+    
     private String password;
 
     @CreationTimestamp
