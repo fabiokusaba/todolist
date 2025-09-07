49036dc7e51c0d5fb97275937e7df956a52ac20e
Fabio Kusaba <fabinhokb44@gmail.com>
2025-09-07 10:15:43 -0300

    feat: add UserModel

---
+++ src/main/java/br/com/fabiokusaba/todolist/user/UserModel.java
@@ -0,0 +1,29 @@
+package br.com.fabiokusaba.todolist.user;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_users")
+public class UserModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String name;
+    private String username;
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+}
