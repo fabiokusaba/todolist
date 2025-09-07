5e4bc65f9d1652ba4fbf3064bfafbd91e4d1ded8
Fabio Kusaba <fabinhokb44@gmail.com>
2025-09-07 20:30:45 -0300

    feat: add getNullPropertyNames

---
+++ src/main/java/br/com/fabiokusaba/todolist/utils/Utils.java
@@ -0,0 +1,31 @@
+package br.com.fabiokusaba.todolist.utils;
+
+import java.beans.PropertyDescriptor;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+
+public class Utils {
+
+    public String[] getNullPropertyNames(Object source) {
+        final BeanWrapper src = new BeanWrapperImpl(source);
+
+        var pds = src.getPropertyDescriptors();
+
+        Set<String> emptyNames = new HashSet<>();
+
+        for (PropertyDescriptor pd : pds) {
+            var srcValue = src.getPropertyValue(pd.getName());
+
+            if (srcValue == null) {
+                emptyNames.add(pd.getName());
+            }
+        }
+
+        var result = new String[emptyNames.size()];
+        return emptyNames.toArray(result);
+    }
+
+}
