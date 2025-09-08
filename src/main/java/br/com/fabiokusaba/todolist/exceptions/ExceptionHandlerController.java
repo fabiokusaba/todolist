30df41543420266dfabfb8176e32465711ee9574
Fabio Kusaba <fabinhokb44@gmail.com>
2025-09-07 21:36:00 -0300

    feat: add ExceptionHandlerController

---
+++ src/main/java/br/com/fabiokusaba/todolist/exceptions/ExceptionHandlerController.java
@@ -0,0 +1,20 @@
+package br.com.fabiokusaba.todolist.exceptions;
+
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionHandlerController {
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<?> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(Map.of("message", ex.getMostSpecificCause().getMessage()));
+    }
+
+}
